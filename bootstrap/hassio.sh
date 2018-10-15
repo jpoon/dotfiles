8d98f45c83fb74d531a3db651ba853bef2488703
Jason Poon <git@jasonpoon.ca>
2018-10-14 21:42:55 -0700

    zwave symlink

--- bootstrap/hassio.sh
+++ bootstrap/hassio.sh
@@ -4,3 +4,6 @@ sudo -i
 apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat software-properties-common
 curl -sSL https://get.docker.com | sh
 curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install" | bash -s -- -m armv71
+
+# symlink zwave to /dev/zwave
+echo 'SUBSYSTEM=="tty", ACTION=="add", ATTRS{idVendor}=="0658", ATTRS{idProduct}=="0200", SYMLINK+="zwave"' > /etc/udev/rules.d/99-usb-serial.rules
